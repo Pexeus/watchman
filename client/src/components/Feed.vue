100158da6a27d4c4b9753fbd3234252b387bf8f5
Pexeus <liam.benedetti@gmx.ch>
2023-06-04 20:33:08 +0200

    ligma

--- client/src/components/Feed.vue
+++ client/src/components/Feed.vue
@@ -1,11 +1,54 @@
 <template>
-    
+  <div class="feed">
+    <div class="result" v-for="content of feed.content" :key="content.id">
+      <img :src="`https://image.tmdb.org/t/p/w300/${content.poster_path}`" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { reactive, watch } from "vue";
+
 export default {
-    setup() {
-        
-    },
-}
+  props: {
+    content: Object,
+  },
+  setup(props, context) {
+    const feed = reactive({});
+
+    watch(
+      () => props.content,
+      async (content) => {
+        feed.content = content
+      }
+    );
+
+    return { feed };
+  },
+};
 </script>
+
+<style scped>
+.feed {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  position: relative;
+  height: 200px;
+}
+
+.result {
+  margin: 10px;
+  border-radius: 5px;
+  height: 100%;
+  box-shadow: 0px 0px 3px gray;
+  cursor: pointer;
+}
+
+.result img {
+  height: 100%;
+  background-size: cover;
+  border-radius: 5px;
+}
+</style>
--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <input type="text" v-model="search.query" />
-    <div class="results">
-        <div class="result" v-for="result of search.results" :key="result.id">
-            <img :src="`https://image.tmdb.org/t/p/w300/${result.poster_path}`">
-        </div>
-    </div>
+    <Feed :content="search.results"></Feed>
   </div>
 </template>
   
 <script>
 import config from "../config";
 import { reactive, watch } from "vue";
-import axios from "axios"
+import Feed from "./Feed.vue";
 
 
 export default {
   name: "Search",
+  components: {
+    Feed
+  },
   setup() {
-    console.log(config);
     const tmdb = config.interfaces.tmdb
     const search = reactive({
       query: "",
@@ -31,10 +29,7 @@ export default {
       () => search.query,
       async (query, old) => {
         const url = `https://api.themoviedb.org/3/search/multi?query=${query}`;
-
         const res = await tmdb.get(url)
-
-        console.log(res);
 
         search.results = res.data.results
       }
@@ -46,29 +41,4 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-    .results {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        position: relative;
-        height: 200px;
-    }
-
-    .result {
-        margin: 10px;
-        border-radius: 5px;
-        height: 100%;
-        box-shadow: 0px 0px 3px gray;
-        cursor: pointer;
-    }
-
-    .result img {
-        height: 100%;
-        background-size: cover;
-        border-radius: 5px;
-    }
-</style>
   
